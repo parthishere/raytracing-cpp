7a194941fa50ef6980b9781ebdc8ed1e4745d4f8
parthishere <parthishere1234@gmail.com>
2024-06-22 22:21:32 -0600

    gradient effect

--- main.cpp
+++ main.cpp
@@ -20,7 +20,9 @@ std::uniform_int_distribution rgb{0, 255};
 
 color ray_color(const ray &r)
 {
-  return color(0, 0, 0);
+  vec3 unit_direction = unit_vector(r.direction());
+  auto a = 0.5 * (unit_direction.y() + 1.0);
+  return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main()
@@ -32,41 +34,39 @@ int main()
 
   int image_height = image_width / aspect_ratio;
   image_height = image_height < 1 ? 1 : image_height;
-  
-  double focal_length { 1.0 };
-  point3 camera_center {0, 0, 0};
+
+  double focal_length{1.0};
+  point3 camera_center{0, 0, 0};
 
   double viewport_height{2.0};
-  //  we have not used aspect ratio as when computing viewport_width, it's because the value set to aspect_ratio is the ideal ratio, it may not be the actual ratio between image_width and image_height. If image_height was allowed to be real valued—rather than just an integer—then it would be fine to use aspect_ratio. But the actual ratio between image_width and image_height can vary based on two parts of the code. First, image_height is rounded down to the nearest integer, which can increase the ratio. Second, we don't allow image_height to be less than one, which can also change the actual aspect ratio.
+  //  I have not used aspect ratio as when computing viewport_width, it's because the value set to aspect_ratio is the ideal ratio, it may not be the actual ratio between image_width and image_height. If image_height was allowed to be real valued—rather than just an integer—then it would be fine to use aspect_ratio. But the actual ratio between image_width and image_height can vary based on two parts of the code. First, image_height is rounded down to the nearest integer, which can increase the ratio. Second, we don't allow image_height to be less than one, which can also change the actual aspect ratio.
   double viewport_width = viewport_height * (double(image_width) / image_height);
 
-
   // Calculate the vectors across the horizontal and down the vertical viewport edges.
-  vec3 viewport_x_vec {viewport_width, 0, 0};
-  vec3 viewport_y_vec {0, -viewport_height, 0};
+  vec3 viewport_x_vec{viewport_width, 0, 0};
+  vec3 viewport_y_vec{0, -viewport_height, 0};
 
   // Calculate the horizontal and vertical delta vectors from pixel to pixel.
   vec3 pixel_delta_x = viewport_x_vec / image_width;
   vec3 pixel_delta_y = viewport_y_vec / image_height;
 
   // Calculate the location of the upper left pixel.
-  point3 viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_x_vec/2 + ((-1 * viewport_y_vec)/2);
+  point3 viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_x_vec / 2 + ((-1 * viewport_y_vec) / 2);
   point3 pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_x + pixel_delta_y);
 
-  //render
+  // render
   std::cout << "P3\n"
             << image_width << " " << image_height << "\n255\n";
 
-  for (int i = 0; i < image_height; i++)
+  for (int j = 0; j < image_height; j++)
   {
-    std::clog << "\rScanlines remaining: " << (image_height - i) << ' ' << std::flush;
-    for (int j = 0; j < image_width; j++)
+    std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+    for (int i = 0; i < image_width; i++)
     {
       point3 pixel_center = pixel00_loc + (i * pixel_delta_y) + (j * pixel_delta_x);
       vec3 ray_direction = pixel_center - camera_center;
 
       ray r(camera_center, ray_direction);
-
 
       // vec3 pixel_color = vec3(rgb(mt), rgb(mt), rgb(mt)); // random pixels
       vec3 pixel_color = ray_color(r);
