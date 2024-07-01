8d3929d41ad5da8bb5d5cb8d20acf3ff983dfd0e
parthishere <parthishere1234@gmail.com>
2024-07-01 16:17:28 -0600

    working

--- main.cpp
+++ main.cpp
@@ -19,18 +19,20 @@ int main()
   HittableList world;
   auto material_ground = make_shared<Lambertian>(color(0.8, 0.8, 0.0));
   auto material_center = make_shared<Metal>(color(0.1, 0.2, 0.5), 0.0);
-  auto material_left = make_shared<Dielectric>(1.00 / 1.33);
+  auto material_left = make_shared<Dielectric>(1.50);
+  auto material_bubble = make_shared<Dielectric>(1.00 / 1.50);
   auto material_right = make_shared<Metal>(color(0.8, 0.6, 0.2), 1.0);
 
   world.add(make_shared<Sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
   world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
   world.add(make_shared<Sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+  world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
   world.add(make_shared<Sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
 
   Camera cam;
 
   cam.aspect_ratio = 16.0 / 9.0;
-  cam.image_width = 800;
+  cam.image_width = 1200;
   cam.samples_per_pixel = 100;
   cam.max_depth = 50;
 
